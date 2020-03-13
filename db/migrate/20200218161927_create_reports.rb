c8c674ce37747b679ebf4958184915f5c9ad735a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-03-13 10:46:50 -0400

    oops

--- db/migrate/20200218161927_create_reports.rb
+++ db/migrate/20200218161927_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration[6.0]
   def change
     create_table :reports do |t|
       t.string :name
-       t.json :data
+      t.json :data
       t.timestamps
     end
   end
